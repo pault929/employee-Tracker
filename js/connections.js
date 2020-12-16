466125ef93a9cadda664d84083f8bf2608c865a1
Paul T <paultoro101@gmail.com>
2020-12-16 18:39:29 -0500

    added connection.js to HW

---
+++ js/connections.js
@@ -0,0 +1,19 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+   host: "localhost",
+   //your name
+   user: "root",
+   //personal password from mySQL ...hide..
+   password: "",
+   //file name you created in MYSql
+   database: "company_db"
+   
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
